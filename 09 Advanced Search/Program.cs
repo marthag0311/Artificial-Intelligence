8ce4fda9ea030fd736e8ffaf01f8ebd841c4a406
marthag0311 <marageofrey@gmail.com>
2025-08-04 09:48:52 +0200

    Add files via upload

---
+++ 09 Advanced Search/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _09_Advanced_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SVM svm = new SVM();
+            var svm = new SimpleSVM(3);
+            double[][] inputs = { new double[] { 1, 2, 3 }, new double[] { 2, 3, 4 }, new double[] { 3, 3, 5 } };
+            int[] labels = { 1, -1, -1 };
+
+            svm.Train(inputs, labels, 0.01, 1000);
+
+            var prediction = svm.Predict(new double[] { 1.5, 2.5, 3.5 });
+            Console.WriteLine($"Prediction: {prediction}");
+
+            prediction = svm.Predict(new double[] { 3, 3, 3.5 });
+            Console.WriteLine($"Prediction: {prediction}");
+
+            prediction = svm.Predict(new double[] { 2.5, 2, 2.9 });
+            Console.WriteLine($"Prediction: {prediction}");
+        }
+    }
+}
---
+++ 09 Advanced Search/SimpleSVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Advanced_Search
+{
+    public class SimpleSVM
+    {
+        public double[] Weights;
+        public double Bias;
+
+        public SimpleSVM(int dimension)
+        {
+            Weights = new double[dimension];
+        }
+
+        public int Predict(double[] doubles)
+        {
+            double result = DotProduct(Weights, doubles) - Bias;
+            if (result >= 0) return 1;
+            return -1;
+        }
+
+        public void Train(double[][] inputs, int[] labels, double learningrate, int epochs) //v2 = iterations
+        {
+            for (int i = 0; i < epochs; i++)
+            {
+                for (int j = 0; j < inputs.Length; j++)
+                {
+                    double prediction = Predict(inputs[j]);
+                    if (prediction != labels[j])
+                    {
+                        // Update weights and bias
+                        for (int k = 0; k < Weights.Length; k++)
+                        {
+                            Weights[k] += learningrate * labels[j] * inputs[j][k];
+                        }
+                        Bias -= learningrate * labels[j];
+                    }
+
+                }
+            }
+        }
+
+        private double DotProduct(double[] a, double[] b)
+        {
+            return a.Zip(b, (x, y) => x * y).Sum();
+        }
+    }
+}
