d8218eeb07607ca46818da8a068b7a21be137ceb
marthag0311 <marageofrey@gmail.com>
2025-08-04 09:56:12 +0200

    Add files via upload

---
+++ 12 Youtube's Video Recommendations and Genetic Algorithms IMS/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _12_Youtube_s_Video_Recommendations_and_Genetic_Algorithms_IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneticAlgorithm ga = new GeneticAlgorithm(200, 8, 6);
+            ga.Run();
+        }
+    }
+}
