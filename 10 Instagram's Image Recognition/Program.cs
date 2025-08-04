31237eaa03015c1f3540cb982db0cfa2779f9a8d
marthag0311 <marageofrey@gmail.com>
2025-08-04 09:49:34 +0200

    Add files via upload

---
+++ 10 Instagram's Image Recognition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10_Instagram_s_Image_Recognition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] training_input = new double[,] { { 0, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 }, { 0, 1, 1 } };
+            double[,] training_output = Matrix.Transpose(new double[,] { { 0, 1, 1, 0 } });
+
+            var nn = new NN(training_input, training_output);
+
+            Console.WriteLine("Weights before training:");
+            Matrix.Print(nn.Weights);
+
+            nn.Train();
+
+            Console.WriteLine("Weights after training:");
+            Matrix.Print(nn.Weights);
+
+            double[,] output = nn.Predict(new double[,] { { 1, 0, 0 } });
+            Console.WriteLine("Predict [1, 0, 0] => " + output);
+
+            output = nn.Predict(new double[,] { { 0, 0, 0 } });
+            Console.WriteLine("Predict [0,0,1] => " + output);
+        }
+    }    
+}
