cfa4b3ed77711f95eb1c84877d7c3b1da7f4baf6
marthag0311 <marageofrey@gmail.com>
2025-08-04 07:42:31 +0200

    Add files via upload

---
+++ 04 Facebook's News Feed Algorithm/DT.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Facebook_s_News_Feed_Algorithm
+{
+    class Node
+    {
+        public double FeatureThreshold { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int? Label { get; set; }
+
+        public Node(double featureThreshold, Node left, Node right)
+        {
+            FeatureThreshold = featureThreshold;
+            Left = left;
+            Right = right;
+        }
+
+        public Node(int label)
+        {
+            Label = label;
+        }
+    }
+
+    class DT
+    {
+        private int MostFrequent(int[] Y)
+        {
+            int cound0 = 0, count1 = 0;
+            
+            foreach(int i in Y)
+            {
+                if (i == 0) cound0++;
+                else count1++;
+            }
+            if (count0 > count1) return 0;
+            return 1;
+        }
+
+        public Node Train(int[] X, int[] Y, int depth, int maxDepth)
+        {
+            if (depth == maxDepth)
+            {
+                int label = MostFrequent(Y);
+                return new Node(label);
+            }
+
+            double threshold = X.Average();
+
+            list<int> XLeft = new list<int>();
+            list<int> YLeft = new list<int>();
+            list<int> XRight = new list<int>();
+            list<int> YRight = new list<int>();
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                if (X[i] <= threshold)
+                {
+                    XLeft.Add(X[i]);
+                    XLeft.Add(X[i]);
+                }
+                else
+                {
+                    XRight.Add(X[i]);
+                    XRight.Add(X[i]);
+                }
+            }
+            return new Node(threshold, 
+                Train(XLeft.ToArray(), YLeft.ToArray, depth + 1, maxDepth),
+                Train(XRight.ToArray(), YRight.ToArray, depth + 1, maxDepth));
+        }
+
+        public int Predict(Node node, double x)
+        {
+            if (node.Label != null)
+                return node.Label.Value;
+
+            if (x <= node.FeatureThreshold)
+                return Predict(node.Left, x);
+
+            return Predict(node.Right, x);
+        }
+
+        public void Print(Node node, string indent = "", bool isLeftChild = true)
+        {
+            if (node == null) return;
+
+            if (node.Left == null && node.Right == null)
+            {
+                Console.WriteLine($"{indent}Leaf: Class={node.Label}");
+            }
+            else
+            {
+                Console.WriteLine($"{indent}Node: Threshold={node.FeatureThreshold}");
+                indent += isLeftChild ? "|   " : "    ";
+                Console.Write($"{indent}L--> ");
+                Print(node.Left, indent, true);
+                Console.Write($"{indent}R--> ");
+                Print(node.Right, indent, false);
+            }
+        }
+    }
+}
---
+++ 04 Facebook's News Feed Algorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04_Facebook_s_News_Feed_Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] X = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] Y = { 0, 0, 0, 1, 1, 1, 1, 1, 1 };
+
+            DT tree = new DT();
+            Node root = tree.Train(X, Y, 0, 4);
+            tree.Print(root);
+
+            Console.WriteLine($"Predict 2 --> {tree.Predict(root, 2)}");
+            Console.WriteLine($"Predict 5 --> {tree.Predict(root, 5)}");
+        }
+    }
+}
