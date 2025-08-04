aea8d057ff23e1b2f85d634c4f005ee66ead34ca
marthag0311 <marageofrey@gmail.com>
2025-08-04 09:50:58 +0200

    Add files via upload

---
+++ 12 YouTube's Video Recommendations and Genetic Algorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿using DSPS;
+
+namespace _12_YouTube_s_Video_Recommendations_and_Genetic_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneticAlgorithm ga = new GeneticAlgorithm();
+            ga.Run();
+        }
+    }
+}
